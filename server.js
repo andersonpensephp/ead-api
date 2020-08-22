036bceaa7ffa521e64b4f4d3dca19e6e09c222ca
Anderson Reis Souto <andersonpensephp@hotmail.com>
2020-08-22 14:36:40 -0300

    chore: WIP Setup

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const app = new Koa();
 const router = new KoaRouter();
 const PORT = 3000;
 
-router.get( '/', ctx => {
-    ctx.body = 'Ead platform'
+router.get('/', (ctx) => {
+  ctx.body = 'Ead platform';
 });
 
 app
-.use(router.routes())
-.use(router.allowedMethods());
-
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(PORT, () => console.log(`Listen port: ${PORT}`));
